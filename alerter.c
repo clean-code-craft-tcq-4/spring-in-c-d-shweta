66b170c55488c34dde56c22afcb1d82d74e1c89d
d-shweta <105386129+d-shweta@users.noreply.github.com>
2022-05-22 17:57:17 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,1 +1,20 @@
+#include "alerter.h"
+#include "stats.h"
+
+
+void check_and_alert(const float maxThreshold, alerter_funcptr alerters, Stats computedStats){
+  
+  if ( computedStats.max > maxThreshold){
+       emailAlerter();
+       ledAlerter();
+  } 
+}
+  
+void emailAlerter(){  
+     emailAlertCallCount=1;
+  }
+  
+void ledAlerter(){
+    ledAlertCallCount=1;
+  }
 
